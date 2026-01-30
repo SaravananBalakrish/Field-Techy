9c28688e63b17932e25ee62b2b2546467addb010
harikrishnan2326 <harikriz22420@gmail.com>
2026-01-30 11:25:39 +0530

    client dashboard page started

--- lib/features/client/dashboard/presentation/pages/client_dashboard_page.dart
+++ lib/features/client/dashboard/presentation/pages/client_dashboard_page.dart
@@ -5,8 +5,71 @@ class ClientDashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Client Dashboard'),
+    final theme = Theme.of(context);
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 24),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              spacing: 10,
+              children: [
+                Text(
+                  'Job Overview',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                  textAlign: TextAlign.start,
+                ),
+                _buildJobCard(
+                    Icons.work,
+                    'Active Jobs',
+                    'Description',
+                    theme.colorScheme.secondary,
+                    context,
+                    trailing:  FilledButton(onPressed: () {}, child: Text('View Jobs')),
+                ),
+                _buildJobCard(Icons.work, 'Completed Jobs', 'Description', Colors.greenAccent, context,),
+                _buildJobCard(Icons.work, 'Jobs Cancelled', 'Description', Colors.white, context,),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildJobCard(IconData icon, String title, String subtitle, Color color, BuildContext context, {Widget? trailing}) {
+    return  Card(
+      elevation: 3,
+      color: color,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          children: [
+            Column(
+              spacing: 10,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  spacing: 10,
+                  children: [
+                    Icon(icon),
+                    Text(title, style: Theme.of(context).textTheme.headlineMedium),
+                  ],
+                ),
+                Text(subtitle, style: Theme.of(context).textTheme.bodyMedium),
+              ],
+            ),
+            const Spacer(),
+            trailing ?? const SizedBox.shrink(),
+          ],
+        ),
+      ),
     );
   }
 }
